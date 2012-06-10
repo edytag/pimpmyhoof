d62f7815c549822e40b5aa441e02fb5b2828074c
Marek Nowak <mareknowakk@gmail.com>
2012-06-10 19:37:21 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pimpmyhoof::Application.config.secret_token = 'a21efcd14e7b8a33fc5f5997efe194fb358e3d2b2f43afba241149ecf9b07dfb45354361b52a6934c70e67c3f26e0755633a6134188571d6d1cc8de5e5bc2d76'
